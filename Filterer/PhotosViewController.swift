b6c99e57074d4b6387296e693dd05b02cd8a8e6f
Bill Tihen <btihen@las.ch>
2017-01-30 22:07:35 +0100

    remembers search terms, clears search terms & reloads list appropriately

--- Filterer/PhotosViewController.swift
+++ Filterer/PhotosViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class PhotosViewController: UITableViewController {
+    
+    var searchTerm: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print( "Photo Controller")
+        
+        // print( "Search Term: " + searchTerm! )
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
     }
 
     override func didReceiveMemoryWarning() {
--- Filterer/TableViewController.swift
+++ Filterer/TableViewController.swift
@@ -9,24 +9,37 @@
 import UIKit
 
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
-
+    
+    var searchTerm: String?
+    var sortedLinks = NSUserDefaults.standardUserDefaults().objectForKey("Links") as? [String] ?? [String]()
+    var refreshControl = UIRefreshControl()
+    
+    @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var termTextField: UITextField!
     
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // assign data source -- state where the data comes from
         tableView.delegate     = self
         tableView.dataSource   = self
         termTextField.delegate = self
+        
     }
-
-    let sortedLinks = NSUserDefaults.standardUserDefaults().objectForKey("Links") as? [String] ?? [String]()
-    // let sortedLinks = unsortedLinks.sort { $0 < $1 }
-    // let sortedLinks = unsortedLinks.sort { $0.unsortedLinks < $1.unsortedLinks }
-    // let sortedLinks = unsortedLinks.sorted { $0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending }
     
+    @IBAction func onClearButton(sender: UIButton) {
+        print("clear")
+        sortedLinks = [String]()
+        print("New Search List")
+        print( sortedLinks )
+        defaults.setObject(sortedLinks, forKey: "Links")
+        self.tableView.reloadData()
+    }
+    
+    // http://stackoverflow.com/questions/37096587/check-textfield-has-value-before-redirecting-to-another-view-swift
     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
         
         if termTextField.text!.isEmpty {
@@ -35,21 +48,31 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         } else{
             print(termTextField.text)
             let searchTerm = termTextField.text!
+            sortedLinks.append( searchTerm )
+            print("New Search Term: ")
             print( searchTerm )
+            print("New Search List")
+            print( sortedLinks )
+            defaults.setObject(sortedLinks, forKey: "Links")
+            self.tableView.reloadData()
             let urlString = "https://api.flickr.com/services/feeds/photos_public.gne?" + searchTerm + "&format=json&nojsoncallback=1"
             print( urlString )
             return true
         }
     }
     
+    func reloadData() {}
+    
     // DATA METHODS
     // how many rows needed
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return sortedLinks.count
     }
     
     // keeps track of which cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        sortedLinks = defaults.objectForKey("Links") as? [String] ?? [String]()
         let cell = tableView.dequeueReusableCellWithIdentifier("LinkCell", forIndexPath: indexPath )
         // populate cell
         cell.textLabel?.text = sortedLinks[indexPath.row]
