c5ffdaa70702e805d0cbe1cab9371c7a4b02fc0a
Bill Tihen <btihen@las.ch>
2017-02-01 10:45:02 +0100

    text data feed works

--- Filterer/PhotoViewController.swift
+++ Filterer/PhotoViewController.swift
@@ -15,14 +15,38 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         "scenery",
         "landscape"
     ]
+    let defaults = NSUserDefaults.standardUserDefaults()
     
-    var searchURL:   String?
+    var urlString:   String?
     var searchTerm:  String?
     var valueToPass: String!
     var imageToPass: UIImage!
     var valuePassed: String?
     
-    let defaults = NSUserDefaults.standardUserDefaults()
+    // FLICKR FEED (html) CODE
+    var feed: Feed? {
+        didSet {
+            print( "FEED - update Tables" )
+            self.tableView.reloadData()
+        }
+    }
+    // keeps track of download sessions (of image downloads)
+    var urlSession: NSURLSession!
+    //
+    weak var dataTask: NSURLSessionDataTask?
+    // create the nsurl session as downloads start
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        self.urlSession = NSURLSession(configuration: configuration)
+    }
+    // destroy nsurl session when view goes away --
+    // don't keep downloading images when not needed!
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.urlSession.invalidateAndCancel()
+        self.urlSession = nil
+    }
     
     @IBOutlet weak var tableView: UITableView!
 
@@ -32,29 +56,48 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         if valuePassed == nil {
             // use last search info
             print( "USING STORED VALUES" )
-            searchURL  = defaults.stringForKey("SearchURL")!
             searchTerm = defaults.stringForKey("SearchTerm")!
         } else {
             // use passed data
             print( "USING PASSED VALUES" )
             searchTerm = valuePassed!
-            searchURL  = "https://api.flickr.com/services/feeds/photos_public.gne?" + searchTerm! + "&format=json&nojsoncallback=1"
         }
-        print( "SEARCH TERM " + searchTerm! )
-        print( "SEARCH URL "  + searchURL! )
+        urlString  = "https://api.flickr.com/services/feeds/photos_public.gne?" + searchTerm! + "&format=json&nojsoncallback=1"
+        print( "FLICKR SEARCH TERM " + searchTerm! )
+        print( "FLICKR SEARCH URL "  + urlString! )
         tableView.delegate     = self
         tableView.dataSource   = self
+        
+        getURL( urlString! )
     }
 
-    // didn't help
-    // http://stackoverflow.com/questions/26022756/warning-attempt-to-present-on-whose-view-is-not-in-the-window-hierarchy-s
-//    func topMostController() -> UIViewController {
-//        var topController: UIViewController = UIApplication.sharedApplication().keyWindow!.rootViewController!
-//        while (topController.presentedViewController != nil) {
-//            topController = topController.presentedViewController!
-//        }
-//        return topController
-//    }
+    func getURL( urlString: String ) {
+        print( "GET URL FUNC" )
+        print( "using URL: " + urlString )
+        
+        if let url = NSURL(string: urlString) {
+            self.updateFeed(url, completion: { (feed) -> Void in
+                //let viewController = application.windows[0].rootViewController as? PhotoViewController
+                //viewController.feed = feed
+                self.feed = feed
+            })
+        }
+    }
+    func updateFeed(url: NSURL, completion: (feed: Feed?) -> Void) {
+        print( "UPDATE FEED" )
+        let request = NSURLRequest(URL: url)
+        print( " GET URL REQUEST " )
+        print( request )
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if error == nil && data != nil {
+                let feed = Feed(data: data!, sourceURL: url)
+                NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                    completion(feed: feed)
+                })
+            }
+        }
+        task.resume()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -62,15 +105,27 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
+        // return images.count
+        print( "COUNT = " )
+        print( self.feed?.items.count ?? 0 )
+        return self.feed?.items.count ?? 0
     }
     
     // keeps track of which cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("PhotoCell", forIndexPath: indexPath )
-        // Configure the cell...
-        cell.textLabel?.text = "Photo of " + images[indexPath.row]
-        cell.imageView?.image = UIImage( named: images[indexPath.row] )
+        // using static images
+        //cell.textLabel?.text = "Photo of " + images[indexPath.row]
+        //cell.imageView?.image = UIImage( named: images[indexPath.row] )
+        // flickr feed
+        let item = self.feed!.items[indexPath.row]
+        // assign text
+        cell.textLabel?.text = item.title
+        // while using text will just send this image (image is ALWAYS NEED FOR SEGUES!)
+        cell.imageView?.image = UIImage( named: "sample" )
+        
+        // add flickr images soon
+        
         return cell
     }
     
@@ -81,11 +136,19 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     // delegate methods
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        valueToPass = images[indexPath.row]
-        print( "SELECTED IMAGE " + valueToPass )
-        // NSUserDefaults.standardUserDefaults().setObject(selectedImage, forKey: "DefaultImage")
-        // valueToPass = selectedImage
-        imageToPass = UIImage( named: images[indexPath.row] )
+        // valueToPass = images[indexPath.row]
+        //// NSUserDefaults.standardUserDefaults().setObject(selectedImage, forKey: "DefaultImage")
+        //// valueToPass = selectedImage
+        // imageToPass = UIImage( named: images[indexPath.row] )
+        
+        // NEW CODE Flickr Feed
+        let cellData = self.feed!.items[indexPath.row]
+        valueToPass = cellData.title
+        // need to change for flicker - but good testing
+        //imageToPass = cellData.imageView?.image
+        
+        print( "SELECTED IMAGE ROW" + valueToPass + " TITLE" )
+        imageToPass = UIImage( named: "sample" )
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
@@ -93,13 +156,20 @@ class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewD
         if (segue.identifier == "InspectImage") {
             print( "Inspect Image Segue" )
             let selectedRow = self.tableView.indexPathForSelectedRow?.row
-            let valueToPass = images[selectedRow!]
-            let imageToPass = UIImage( named: images[selectedRow!] )
-            print( "PASSING VALUE -- " + valueToPass )
-            // store the new default value
+            
+            // OLD STATIC IMAGES CODE
+            //valueToPass = images[selectedRow!]
+            //imageToPass = UIImage( named: images[selectedRow!] )
+            
+            // NEW CODE Flickr Feed
+            let item = self.feed!.items[selectedRow!]
+            valueToPass = item.title
+            // need to change for flicker - but good testing
+            imageToPass = UIImage( named: "sample" )
+            
+            print( "SEGUE PASSING VALUE -- " + valueToPass + "Title" )
             // initialize new view controller and cast it as your view controller
             let nextController = segue.destinationViewController as! PhotoInspectViewController
-            // nextController.passedValue  = valueToPass
             nextController.passedImage = imageToPass
         }
     }
